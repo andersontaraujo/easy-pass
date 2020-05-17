e7500eb48bf37c6bf9ec0b581aef9341edd305a7
Anderson Ricardo Targino Araujo <anderson.araujo@compasso.com.br>
2020-05-16 22:46:52 -0300

    added project structure

---
+++ src/main/java/br/com/devaware/easypass/EasyPassApplication.java
@@ -0,0 +1,13 @@
+package br.com.devaware.easypass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devaware/easypass/EasyPassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devaware.easypass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
